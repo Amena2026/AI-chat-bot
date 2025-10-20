d6b2737066f7d130dec96cc15dc1deebeb59e03f
Alex M <menaalex8@gmail.com>
2025-10-20 18:58:55 -0400

    use environment variable for service account

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -1,8 +1,15 @@
-
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+let serviceAccount;
 
-// connects our node.js server to my Firebase project using my firebase key and url
+if (process.env.SERVICE_ACCOUNT_BASE64) {
+  // Production: Decode from base64
+  const serviceAccountJson = Buffer.from(process.env.SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8');
+  serviceAccount = JSON.parse(serviceAccountJson);
+} else {
+  // Local development: Use file
+  serviceAccount = require('../serviceAccountKey.json');
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL
